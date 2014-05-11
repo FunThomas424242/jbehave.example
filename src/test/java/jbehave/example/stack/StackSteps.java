8ebbfe646a0fb377e91222be8ab50b3f9c873c22
FunThomas424242 <funthomas424242@gmail.com>
2014-05-11 18:21:24 +0200

    project structure changed
    Signed-off-by: FunThomas424242 <funthomas424242@gmail.com>

---
+++ src/test/java/jbehave/example/stack/StackSteps.java
@@ -0,0 +1,45 @@
+package jbehave.example.stack;
+
+import java.util.Stack;
+
+import org.jbehave.core.annotations.Given;
+import org.jbehave.core.annotations.Then;
+import org.jbehave.core.annotations.When;
+import org.jbehave.core.steps.Steps;
+import org.junit.Assert;
+
+public class StackSteps extends Steps {
+
+    private Stack<String> stackUnderTest;
+    private String searchElement;
+
+    @Given("sei ein leerer Stack.")
+    public void anEmptyStack() {
+        this.stackUnderTest = new Stack<String>();
+    }
+
+    @When("das Wort $element hinzugefügt wird")
+    public void anElementIsAdded(final String element) {
+        this.stackUnderTest.push(element);
+    }
+
+    @When("das oberste Element vom Stack entfernt wird")
+    public void removeLastElement() {
+        this.stackUnderTest.pop();
+    }
+
+    @When("das Wort $element gesucht wird")
+    public void searchForElement(final String element) {
+        this.searchElement = element;
+    }
+
+    @Then("liegt das Wort $word ganz oben auf dem Stack.")
+    public void theResultingElementShouldBe(final String word) {
+        Assert.assertEquals(this.stackUnderTest.search(word), 1);
+    }
+
+    @Then("ist die erwartete Position $pos.")
+    public void thePositionReturnedShouldBe(final int pos) {
+        Assert.assertEquals(this.stackUnderTest.search(this.searchElement), pos);
+    }
+}
---
+++ src/test/java/jbehave/example/stack/StackSzenario.story
@@ -0,0 +1,23 @@
+Erzählung:
+Um in einem Java Programm einen Stapel von Dingen abarbeiten zu können
+Als Entwickler
+Möchte ich Schnittstellen und Implementierungen aus Java direkt nutzen
+Damit ich nicht immer eine eigene Stackverwaltung bauen muss.
+ 
+ 
+Szenario:  Grundlegende Stack Funktionen
+ 
+Gegeben sei ein leerer Stack.
+Wenn das Wort Java hinzugefügt wird
+Und das Wort C++ hinzugefügt wird
+Wenn das oberste Element vom Stack entfernt wird
+Dann liegt das Wort Java ganz oben auf dem Stack.
+
+Szenario:  Stack Suche
+ 
+Gegeben sei ein leerer Stack.
+Wenn das Wort Java hinzugefügt wird
+Und das Wort C++ hinzugefügt wird
+Wenn das Wort PHP hinzugefügt wird
+Und das Wort Java gesucht wird
+Dann ist die erwartete Position 3.
